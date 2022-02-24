9843abd54bed6cd1734f5d7aa798f15adde8d79b
Ender-Wiggin <117010097@link.cuhk.edu.cn>
2022-02-25 03:09:19 +0800

    add img

--- tfm.py
+++ tfm.py
@@ -1,12 +1,54 @@
-import pandas as pd
-# from config import Config as cfg
-# import pgOperation
-import matplotlib.pyplot as plt
 import streamlit as st
+import hydralit_components as hc
+import datetime
+import pandas as pd
+
+# specify the primary menu definition
+st.set_page_config(layout='wide',initial_sidebar_state='collapsed',)
+
+# specify the menu definition we'll stick in the sidebar
+side_menu_data = [
+    {'icon': "far fa-copy", 'label':"Left End",'ttip':"I'm the Left End tooltip!"}, #can specify an icon from the bootstrap icon library
+    {'icon': "far fa-copy", 'label':"Copy"},
+    {'label':"Chart"}, # the minimum we can specify for a menu item
+    {'id':'special return value here','icon': "far fa-address-book", 'label':"Book"}, #can provide a special id to return when clicked
+    {'icon': "far fa-calendar-alt", 'label':"Calendar"}, #or can let the label be the return value when clicked
+    {'icon':"🐙",'label':"Component",'ttip':"I'm the Component tooltip!"}, # can als use an emoji as the icon
+    {'icon': "fas fa-tachometer-alt", 'label':"Dashboard"},
+    {'label':"Da55shboard"}, # or no icon
+    {'icon':'🤵','label':"Right End"},
+]
+
+# specify the primary menu definition
+menu_data = [
+    {'icon': "far fa-copy", 'label':"Left End"},
+    {'id':'Copy','icon':"🐙",'label':"Copy"},
+    {'icon': "far fa-chart-bar", 'label':"Chart"},#no tooltip message
+    {'icon': "far fa-address-book", 'label':"Book"},
+    {'id':' Crazy return value 💀','icon': "💀", 'label':"Calendar"},
+    {'icon': "far fa-clone", 'label':"Component"},
+    {'icon': "fas fa-tachometer-alt", 'label':"Dashboard",'ttip':"I'm the Dashboard tooltip!"}, #can add a tooltip message
+    {'icon': "far fa-copy", 'label':"Right End"},
+]
+
+# we can override any part of the primary colors of the menu
+#over_theme = {'txc_inactive': '#FFFFFF','menu_background':'red','txc_active':'yellow','option_active':'blue'}
+over_theme = {'txc_inactive': '#FFFFFF'}
+menu_id = hc.nav_bar(menu_definition=menu_data,home_name='Home',override_theme=over_theme)
+
+with st.sidebar:
+    side_menu_id = hc.nav_bar(menu_definition=side_menu_data,key='sidetbar',login_name='Login',override_theme=over_theme,first_select=6)
+
+#get the id of the menu item clicked
+st.info(f"{menu_id=}")
+st.info(f"{side_menu_id=}")
+
+
+
 ori = pd.read_csv('preprocess.csv')
 ori['createtime'] = pd.to_datetime(ori['createtime'])
 
-playerNum = st.radio("选择玩家人数", ['2P', '4P'])
+playerNum = st.sidebar.selectbox("选择玩家人数", ['2P', '4P'], index=1)
 
 if playerNum == '2P':
     playerNum = 2
@@ -21,7 +63,7 @@ st.write('<style>div.row-widget.stRadio > div{flex-direction:row;vertical-align:
 # st.write('<style>div.st-bf{flex-direction:column;} div.st-ag{font-weight:bold;padding-left:2px;}</style>', unsafe_allow_html=True)
 delete_list = {'界限突破': 'breakthrough', '阿瑞斯扩': 'aresExtension', '群友扩': 'erosCardsOption', '双公司': 'doubleCorp', '探路者扩': 'pathfindersExpansion'}
 # game_options = st.multiselect("game options", delete_list.values())
-with st.expander("选择游戏设置"):
+with st.sidebar.expander("选择游戏设置"):
     for i in delete_list.values():
         exec("%s = st.radio('%s', ('全选', '开启', '禁用'))"%(i,list(delete_list.keys())[list(delete_list.values()).index(i)]))
 
@@ -77,7 +119,7 @@ def getPlayerNumResult(df, player_num = 4):
         else:
             mid = pd.concat([df,player_df.reindex(df.index)],axis=1)
             res = pd.concat([res, mid],axis=0, ignore_index=True)
-            print((mid.loc[pd.isna(mid['player']) == False]).shape[0])
+            # print((mid.loc[pd.isna(mid['player']) == False]).shape[0])
         # df = pd.concat([df, pd.json_normalize(df[player_idx])],axis=1)
     res.drop(['player'+str(i) for i in range(1, 7)], axis=1, inplace=True)
     res['count'] = 1
