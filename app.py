605e48e7db2269d555023f6b50b8f59fdc971069
Ender-Wiggin2019 <117010097@link.cuhk.edu.cn>
2022-07-07 17:40:49 +0800

    test multi

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 import streamlit as st
+import numpy as np
+import hashlib
+# import hydralit_components as hc
+import datetime
+import pandas as pd
+from PIL import Image
+import matplotlib.pyplot as plt
+import altair as alt
+import base64
+import sqlite3
 
-def main_page():
-    st.markdown("# Main page 🎈")
-    st.sidebar.markdown("# Main page 🎈")
+st.set_page_config(page_title= '殖民火星数据',page_icon='🔥', initial_sidebar_state='auto',)
 
-def page2():
-    st.markdown("# Page 2 ❄️")
-    st.sidebar.markdown("# Page 2 ❄️")
+# @st.experimental_singleton
+# def get_db_sessionmaker():
+#     ana = sqlite3.connect('./game_analysis.db', check_same_thread=False)
+#     return ana
 
-def page3():
-    st.markdown("# Page 3 🎉")
-    st.sidebar.markdown("# Page 3 🎉")
-
-page_names_to_funcs = {
-    "Main Page": main_page,
-    "Page 2": page2,
-    "Page 3": page3,
-}
-
-selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
-page_names_to_funcs[selected_page]()
+# con = get_db_sessionmaker()
+st.markdown("# Main page 🎈")
+st.sidebar.markdown("# Main page 🎈")
